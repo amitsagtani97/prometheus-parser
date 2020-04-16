f50c1ec5d6d7c8c393e8120384dd851e08d1a7c5
Amit Sagtani <asagtani06@gmail.com>
2020-04-16 18:53:16 +0530

    added get_metric function

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import requests
 
-
-def get_metrics_list():
+def get_available_time_series():
     url = "http://localhost:9090/api/v1/label/__name__/values"
     r = requests.get(url)
     #print(r.json()['data'])
 
-    return r.json()['data'][:20]
+    return r.json()['data']
 
-def get_metrics(metric):
-    #url = http://localhost:9090/
-    pass
+def get_time_series_metric(name):
+    baseurl = "http://localhost:9090/api/v1/"
+    url = baseurl + "query?query=" + name
+    r = requests.get(url).json()
+    #print(r)
 
-print(get_metrics_list())
+    if r['status'] == "success":
+        resultType = r['data']['resultType']
+        print("Type of result : " + resultType)
+        return r['data']['result']
+
+print(get_available_time_series())
+
+print(get_time_series_metric("node_time_seconds"))
